17e7a76279b60717d6055fbe374674e8b2432ed3
Juan Manuel Gutierrez <gutierrezmanueljuan@outlook.com>
2024-02-27 10:42:57 -0300

    cambio

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express = require("express"); 
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const FileStore = require("session-file-store");
+//No se olviden de inicializarlo!:
+const fileStore = FileStore(session);
+const MongoStore = require("connect-mongo");
+//const userRouter = require("./routes/user.router.js");
+//const sessionRouter = require("./routes/sessions.router.js");
 
 // import express from "express";
 // import cookieParser from "cookie-parser";
@@ -14,12 +20,48 @@ const miAltaClaveSecreta = "TinkiWinki";
 app.use(cookieParser(miAltaClaveSecreta));
 //Modificación que hacemos para firmar cookies. 
 app.use(session({
+    //1) Creamos una sesion con Memory Storage: 
     secret:"secretCoder",
-    resave:true,
-    //Esta configuracion me permite mantener activa la sesión frente a la inactividad del usuario. 
-    saveUninitialized:true
-    //Me permite guardar cualquier sesión aún cuando el objeto de sesión no tenga nada para contener. 
+    //Es el valor para firmar la cookie. 
+    resave: true, 
+    //Esta config me permite mantener la sesión activa frente a la inactividad del usuario. 
+    saveUninitialized:true,
+    //Me permite guardar cualquier sesión aun cuando el objeto de sesión no tenga nada para contener. 
+
+
+    //2) Utilizando File Storage:
+    //store: new fileStore({path:"./src/sessions", ttl: 15, retries: 1}),
+    //path: la ruta de donde se van a guardar los archivitos de sesión. 
+    //ttl: Time To Live (en segundos va!)
+    //retries: cantidad de vecesque el servidor tratara de leer el archivo. 
+
+    //3) Utilizando Mongo Storage: 
+
+    store: MongoStore.create({
+        mongoUrl: "mongodb+srv://gutierrezmanueljuan:jmg35960568982@cluster0.vuvwwx5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", ttl: 100
+    })
+
 }))
+//app.use("/api/users", userRouter);
+//app.use("/api/sessions", sessionRouter);
+
+//Login de usuario con Session: 
+
+app.get("/login", (req, res) => {
+    let usuario = req.query.usuario; 
+
+    req.session.usuario = usuario; 
+    res.send("Guardamos el usuario por medio de query");
+})
+
+//Verificamos el usuario:
+
+app.get("/usuario", (req, res) => {
+    if(req.session.usuario) {
+        return res.send(`El usuario registrado es el siguiente: ${req.session.usuario} `);
+    }
+    res.send("No tenemos un usuario registrado, vamos a morir");
+})
 
 //Middleware de autenticación: 
 
